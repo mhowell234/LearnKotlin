95722f8d482739f1d336946d371fc4d2523f3674
Matt Howell <mhowell234@gmail.com>
2022-12-23 19:49:21 -0500

    Initial Commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+fun main() {
+
+    val fruit = arrayOf("Apple", "Banana", "Cherry", "Blueberry", "Pomegranate")
+    val index = arrayOf(1, 3, 4, 2)
+
+    var x = 0
+    while (x < index.size) {
+        println("Fruit = ${fruit[index[x]]}")
+        x += 1
+    }
+
+    index.forEach { println("Fruit = ${fruit[it]}") }
+
+    println(max(4, 9))
+    println(max(10, 2))
+
+    println(getGameChoice(arrayOf("Matt", "Marnie", "TyTy", "Linda", "Preston", "Charlie", "Lexi")))
+}
+
+fun max(a: Int, b: Int) = if (a > b) a else b
+
+fun getGameChoice(optionsParam: Array<String>) = optionsParam[(Math.random() * optionsParam.size).toInt()]
